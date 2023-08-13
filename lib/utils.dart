52042c6c93e1486374a671357e681d3c094bdf1b
Mike Sheldon <mike@mikeasoft.com>
2023-08-13 15:13:58 +0100

    Add missing utils.dart

---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+String? getHome() {
+  return Platform.environment.containsKey("SNAP_REAL_HOME")
+      ? Platform.environment['SNAP_REAL_HOME']!
+      : Platform.environment['HOME'];
+}
+
+Future<Directory> getDataDir() async {
+  if (Platform.environment.containsKey("SNAP_USER_COMMON")) {
+    return Future.value(Directory(Platform.environment['SNAP_USER_COMMON']!));
+  }
+  return getApplicationSupportDirectory();
+}
