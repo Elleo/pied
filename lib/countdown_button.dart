0a097daf10beaca26f549e18d276a2f2fb51f789
Mike Sheldon <mike@mikeasoft.com>
2023-11-19 18:21:47 +0000

    Add missing source files

---
+++ lib/countdown_button.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class CountdownButton extends StatefulWidget {
+  const CountdownButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  _CountdownButtonState createState() => new _CountdownButtonState();
+}
+
+class _CountdownButtonState extends State<CountdownButton> {
+  int revertTime = 60;
+  late Timer revertTimer;
+
+  _CountdownButtonState() {
+    revertTime = 60;
+    revertTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        revertTime--;
+      });
+      if (revertTime == 0) {
+        revertTimer.cancel();
+        widget.onPressed();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        child: Text('Revert to original configuration ($revertTime)'),
+        onPressed: () {
+          revertTimer.cancel();
+          widget.onPressed();
+        });
+  }
+}
