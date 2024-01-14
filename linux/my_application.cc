77cf7b9e1090d03e1c5d4c4e7fd03065ca9e2f80
Mike Sheldon <mike@mikeasoft.com>
2024-01-14 15:48:27 +0000

    Set minimum window size

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -47,7 +47,8 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "Pied");
   }
 
-  gtk_window_set_default_size(window, 440, 600);
+  gtk_window_set_default_size(window, 550, 600);
+  gtk_widget_set_size_request(GTK_WIDGET(window), 600, 500);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
