dbea66a0487bef556a5d11b33ae374623e89d804
Mike Sheldon <mike@mikeasoft.com>
2024-09-28 22:46:32 +0100

    Update flatpak packaging, build against 24.08 SDK

--- flatpak/build-flatpak.sh
+++ flatpak/build-flatpak.sh
@@ -1,21 +1,21 @@
 set -e
 set -x
 
-ls
+tar -zxf pied-0.2.1-x86_64.tar.gz
 
 mkdir -p /app/pied
-cp -r linux/x64/release/bundle/* /app/pied/
+cp -r pied/* /app/pied/
 
 mkdir -p /app/bin
 ln -s /app/pied/pied /app/bin/pied
 cp killall /app/bin/
-cp spd-say /app/bin/
 
-mkdir -p /app/share/icons/
-cp -r com.mikeasoft.pied.png /app/share/icons/
+mkdir -p /app/share/icons/hicolor/256x256/apps/
+cp -r com.mikeasoft.pied.png /app/share/icons/hicolor/256x256/apps/
 
 mkdir -p /app/share/applications
 cp -r com.mikeasoft.pied.desktop /app/share/applications/
 
 mkdir -p /app/share/metainfo
 cp -r com.mikeasoft.pied.metainfo.xml /app/share/metainfo/
+
